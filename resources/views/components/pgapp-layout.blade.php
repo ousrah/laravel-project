7fb31a9add9665da78078572ab6a92103f4c144a
rahmouni oussama <ousrah@gmail.com>
2023-03-08 10:26:50 +0100

    power grid config and usage

--- resources/views/components/pgapp-layout.blade.php
+++ resources/views/components/pgapp-layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- pg -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,9 +13,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Styles -->
         @livewireStyles
-
+        @powerGridStyles
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/alp.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -36,7 +37,7 @@
         </div>
         <!-- Scripts -->
         @livewireScripts
-
+        @powerGridScripts
 
     </body>
 </html>
